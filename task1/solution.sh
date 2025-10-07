737e2d794b5bc51320b779af774bb8cde53bdae2
aaaalfo <antonmaygkov30@gmail.com>
2025-10-07 21:57:29 +0000

    fixTask1

--- task1/solution.sh
+++ task1/solution.sh
@@ -1,8 +1,7 @@
+set -e
+
 DIR="$1"
 SEARCH="$2"
 REPLACE="$3"
 
-
-find "$DIR" -type f -name "*.txt" | while read -r file; do
-    sed -i '' "s/${SEARCH//\//\\/}/${REPLACE//\//\\/}/g" "$file"
-done
+find "$DIR" -type f -name "*.txt" -exec sed -i '' "s/${SEARCH//\//\\/}/${REPLACE//\//\\/}/g" {} +
