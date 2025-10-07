596609b0ff40851936e7699885ebf4a308574b45
aaaalfo <antonmaygkov30@gmail.com>
2025-10-07 22:03:13 +0000

    Task2Fix

--- task2/solution.sh
+++ task2/solution.sh
@@ -6,14 +6,10 @@ dir_count=$(find "$DIR" -type d | wc -l)
 
 largest_file=$(find "$DIR" -type f -exec du -b {} + | sort -nr | head -n 1)
 
-if [ -z "$largest_file_info" ]; then
-    largest_file_name=""
-    largest_file_size=0
-else
-    largest_file_size=$(echo "$largest_file_info" | awk '{print $1}')
-    largest_file_name=$(echo "$largest_file_info" | cut -f2-)
-fi
+largest_file_size=$(echo "$largest_file" | awk '{print $1}')
+largest_file_name=$(echo "$largest_file" | cut -f2-)
 
-echo "$file_count"
-echo "$dir_count"
-echo "$largest_file_name, $largest_file_size байт"
+
+echo "Files: $file_count"
+echo "Dirs: $dir_count"
+echo "Largest file: $largest_file_name, $largest_file_size bytes"
