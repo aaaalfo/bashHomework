ab590327e841e6c32e285fca03f892a488b1e5e4
aaaalfo <antonmaygkov30@gmail.com>
2025-10-07 20:45:54 +0000

    fix2

--- task1/solution.sh
+++ task1/solution.sh
@@ -1,6 +1,6 @@
 path = "$1"
-string_for_find = "$2"
-string_for_change = "$3"
+search = "$2"
+replace = "$3"
 find path -type f -name "*.txt" | while read -r file; do
-    sed -i "s/${string_for_find}/${string_for_change}/g" "$file"
+    sed -i '' "s/${searh//\//\\/}/${replace//\//\\/}/g" "$file"
 done
