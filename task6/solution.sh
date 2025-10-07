28cd44e6f28b61290ce15f28a8a3f46668b6b0ed
aaaalfo <antonmaygkov30@gmail.com>
2025-10-07 21:53:47 +0000

    fix2Task6

--- task6/solution.sh
+++ task6/solution.sh
@@ -1,4 +1,7 @@
 
+
 USER_NAME=$(whoami)
 
-echo ps aux | grep "^$USER_NAME" | sort -rk 4 | 
+echo "USER     PID     %CPU   %MEM   COMM"
+
+ps aux | grep "^$USER_NAME" | sort -rk 4 | awk '{printf "%-8s %-8s %-6s %-6s %s\n", $1, $2, $3, $4, $11}'
