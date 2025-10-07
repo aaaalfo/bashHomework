c9ca9bc7a9b81ec11369bc08dcf3a82ededcb958
aaaalfo <antonmaygkov30@gmail.com>
2025-10-07 21:59:25 +0000

    anotherTask1Fix

--- task1/solution.sh
+++ task1/solution.sh
@@ -4,4 +4,4 @@ DIR="$1"
 SEARCH="$2"
 REPLACE="$3"
 
-find "$DIR" -type f -name "*.txt" -exec sed -i '' "s/${SEARCH//\//\\/}/${REPLACE//\//\\/}/g" {} +
+find "$DIR" -type f -name "*.txt" -exec sed -i "s/${SEARCH//\//\\/}/${REPLACE//\//\\/}/g" {} +
